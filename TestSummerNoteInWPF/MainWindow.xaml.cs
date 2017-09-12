82fa2903f578976589018ca67d1164ea991fdf7e
Donma Stark <Donma Stark>
2017-09-12 14:25:55 +0800

    加入專案檔案。

---
+++ TestSummerNoteInWPF/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestSummerNoteInWPF
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
+            var htmlAgent = new HtmlAgent();
+            wb1.ObjectForScripting = htmlAgent;
+
+
+            string curDir = Directory.GetCurrentDirectory();
+            wb1.Navigated += Wb1_Navigated;
+            this.wb1.Navigate(new Uri(String.Format("file:///{0}/Assets/sample.html", curDir)));
+
+
+
+
+        }
+
+        private void Wb1_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+
+
+    }
+
+    //[PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+    [ComVisible(true)]
+    public class HtmlAgent
+    {
+        public void ShowMessageFromHTML(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+
+        public string UploadImage(string img)
+        {
+
+            var imageBase64=img.Split(new string[] { ";base64," }, StringSplitOptions.None)[1];
+          
+          
+            var image = Base64ToImage(imageBase64).Source as BitmapSource;
+            var filename = Guid.NewGuid().ToString("N");
+           
+            using (var fileStream = new FileStream(AppDomain.CurrentDomain.BaseDirectory +"Assets"+ System.IO.Path.DirectorySeparatorChar + filename + ".png", FileMode.Create))
+            {
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(image));
+                encoder.Save(fileStream);
+            }
+
+            return filename + ".png";
+        }
+
+
+        public Image Base64ToImage(string base64String)
+        {
+            // Convert base 64 string to byte[]
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            // Convert byte[] to Image
+            using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+            {
+                Image image = new Image();
+                using (MemoryStream stream = new MemoryStream(imageBytes))
+                {
+                    image.Source = BitmapFrame.Create(stream,
+                                                      BitmapCreateOptions.None,
+                                                      BitmapCacheOption.OnLoad);
+                }
+
+                return image;
+            }
+        }
+
+        private void SaveFile(Stream stream, FileStream fs)
+        {
+            try
+            {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    fs.Write(buffer, 0, bytesRead);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error:" + ex.Message);
+            }
+        }
+
+    }
+
+
+}
